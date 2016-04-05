e0b3fc20d9a8742257a668d6f61afdcbee9393ee
zaheelee <zaheelee@gmail.com>
2016-04-05 12:27:47 -0400

    finished creating the tables

--- FinalProject.sql
+++ FinalProject.sql
@@ -3,6 +3,10 @@ CREATE TABLE Team(name VARCHAR(24) NOT NULL,
 					universe VARCHAR(24),
 			PRIMARY KEY(name));
 
+CREATE TABLE Power(name VARCHAR(24) NOT NULL,
+					origin VARCHAR(24),
+			PRIMARY KEY(name));
+
 CREATE TABLE Superhero(name VARCHAR(24) NOT NULL,
 						secret_identity VARCHAR(24),
 						universe VARCHAR(24),
@@ -16,6 +20,18 @@ CREATE TABLE Superhero(name VARCHAR(24) NOT NULL,
 				PRIMARY KEY(name),
 				FOREIGN KEY(sidekick) REFERENCES Superhero(name),
 				FOREIGN KEY(team_name) REFERENCES Team(name));
+				
+CREATE TABLE HasPower(power_name VARCHAR(24) NOT NULL,
+						name VARCHAR(24) NOT NULL,
+				PRIMARY KEY(power_name),
+				FOREIGN KEY(name) REFERENCES Superhero(name),
+				FOREIGN KEY(power_name) REFERENCES Power(name));
+				
+CREATE TABLE Archenemies(hero_name VARCHAR(24) NOT NULL,
+							villain_name VARCHAR(24) NOT NULL,
+					PRIMARY KEY(hero_name),
+					FOREIGN KEY(hero_name) REFERENCES Superhero(name),
+					FOREIGN KEY(villain_name) REFERENCES Superhero(name));
 				
 
 INSERT INTO Team
