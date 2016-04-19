54c262ea10eb43199d7ac530c78f879d627da409
Katherine Cass <cass@case.edu>
2016-04-19 14:22:10 -0400

    application working, run by typing ruby application.rb

--- application.rb
+++ application.rb
@@ -13,15 +13,39 @@ class Application
     @db_connection.query("USE SuperheroDatabase")
   end
 
+  def start
+    @on = true
+    puts "\nSelect an option:"
+    puts "1. Get secrete identity from name"
+    puts "quit"
+    choice = gets.chomp 
+    task_router(choice)
+    if @on 
+       start
+    end
+  end
+
+  def task_router(n)
+    case n
+      when "1"
+        puts "Enter a superhero name: " 
+        name = gets.chomp
+        get_secret_identity_from_name(name)
+      when "quit"
+        @on = false
+      else 
+        puts "invalid choice, try again, bro."
+    end
+  end
+
   def get_secret_identity_from_name(name)
      query="SELECT * FROM " + @superhero_table + " WHERE name=\"" + name + "\";"
-     print query + "\n"
      results = @db_connection.query(query)
      read_rows("secret_identity", results)
   end
 
   def read_rows(attr, results)
-     print "Reading Results: \n"
+     print "\nResults: \n"
      results.each do |row|
          if !row[attr].nil?
            print row[attr] + "\n"
@@ -31,4 +55,4 @@ class Application
 end
 
  app = Application.new
- app.get_secret_identity_from_name("Iron Man")
+ app.start
